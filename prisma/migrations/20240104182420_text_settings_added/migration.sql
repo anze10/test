bfc53f2535c5da33243fd5f0480926e1e31e2bf1
Anže Repše <anze.repse@gmail.com>
2024-01-04 20:45:35 +0100

    mogoce dela

--- prisma/migrations/20240104182420_text_settings_added/migration.sql
+++ prisma/migrations/20240104182420_text_settings_added/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
-CREATE TABLE `meteoriti` (
-    `id_meteorit` INTEGER NOT NULL AUTO_INCREMENT,
-    `trajanje` DECIMAL(10, 2) NOT NULL,
-    `dan` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `cas` TIME NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE
+    `meteoriti` (
+        `id_meteorit` INTEGER NOT NULL AUTO_INCREMENT,
+        `trajanje` DECIMAL(10, 2) NOT NULL,
+        `dan` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+        `cas` TIME NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    PRIMARY KEY (`id_meteorit`)
+PRIMARY KEY (`id_meteorit`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
