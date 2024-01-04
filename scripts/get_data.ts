63d4cd34a55662bb034215c0e359b7eb6ea147f8
Anže Repše <anze.repse@gmail.com>
2024-01-04 18:51:56 +0100

    dela?

--- scripts/get_data.ts
+++ scripts/get_data.ts
@@ -1,8 +1,8 @@
 import { db } from "~/server/db";
 
 const main = async () => {
-    const meteoriti = await db.meteoriti.findMany({})
-    console.log("meteoriti", meteoriti)
+    const meteorit = await db.meteorit.findMany({})
+    console.log("meteoriti", meteorit)
 }
 
 await main()
