d3c2bd95359975042aaf2c0b4af805523a4a674e
Anže Repše <anze.repse@gmail.com>
2024-01-08 16:21:04 +0100

    dela :)

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,25 +1,29 @@
+"use client"
 import React from 'react';
 import { Typography, Link, Box } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 const Footer = async () => {
     return (
-        <Box component="footer" sx={{ textAlign: 'center', padding: '20px 0' }}>
-            <Typography>
-                Spletno stran je izdelal jst s pomočjo dveh prijateljev, katerima se iskreno zahvaljujem: luka in Tim."
+        <Box component="footer" sx={{ textAlign: 'center', padding: '20px 0', backgroundColor: "black" }}>
+            <Typography sx={{ color: "white" }}>
+                @Scidrom 2024 vse pravice pridržane
             </Typography>
-            <Link href="https://github.com/anze10" color="inherit">
+            <Typography sx={{ color: "white" }}>
+                Spletno stran je izdelal jst s pomočjo dveh prijateljev, katerima se iskreno zahvaljujem: luka in Tim.
+            </Typography>
+            <Link href="https://github.com/anze10" color="inherit" sx={{ color: "white" }}>
                 <GitHubIcon sx={{ marginRight: 8 }} />
-                Profile 1
+                Anže
             </Link>
             {' | '}
-            <Link href="https://github.com/lukaprsina" color="inherit">
+            <Link href="https://github.com/lukaprsina" color="inherit" sx={{ color: "white" }}>
                 <GitHubIcon sx={{ marginRight: 8 }} />
-                Profile 1
+                Luka
             </Link>
             {' | '}
-            <Link href="timnahtigal" color="inherit">
+            <Link href="timnahtigal" color="inherit" sx={{ color: "white" }}>
                 <GitHubIcon sx={{ marginRight: 8 }} />
-                Profile 1
+                Tim
             </Link>
 
 
